1a0e99ba84d982622603fde6776f8d779ca5c59d
Md Badiuzzaman Pranto <prantoamt@gmail.com>
2023-01-22 00:16:59 +0100

    initial models created

---
+++ assignment/api/urls.py
@@ -0,0 +1,10 @@
+from rest_framework.authtoken import views
+from django.urls import path, include
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token)
+]
