f60182f6c6d92b3fdfa541576e0954a284ed2570
Md Badiuzzaman Pranto <prantoamt@gmail.com>
2023-01-22 10:29:23 +0100

    Company Product DB created

--- assignment/company/models.py
+++ assignment/company/models.py
@@ -5,6 +5,7 @@ from django.db import models
 # Third party imports
 
 # Self imports
+from product.models import Product
 
 
 class Company(models.Model):
@@ -28,3 +29,24 @@ class Company(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class CompanyProduct(models.Model):
+    iid = models.UUIDField(
+        db_column="company_product_id",
+        verbose_name="Company Product ID",
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
+    company = models.ForeignKey(
+        'company.Company', on_delete=models.CASCADE, verbose_name='Company', related_name='products')
+    product = models.ForeignKey(
+        'product.Product', on_delete=models.CASCADE, verbose_name='Product', related_name='companies')
+    
+    class Meta:
+        verbose_name_plural = 'Company products'
+        db_table = 'CompanyProducts'
+
+    def __str__(self) -> str:
+        return self.company.name
